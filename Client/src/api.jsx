c3b15e5f12f6105502c75c73d13ab72171dc1657
Arghya dey <159042843+arghya55@users.noreply.github.com>
2026-05-05 13:51:34 +0530

    Change API base URL to production endpoint

--- Client/src/api.jsx
+++ Client/src/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://utkal-policy-backend.onrender.com",
 });
 
 // 🔥 AUTO TOKEN ATTACH
