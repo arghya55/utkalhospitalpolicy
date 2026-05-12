4c8a9213cd347246aa9323696b9b1d9fbe9eed0c
Arghya <arghyadey043@gmail.com>
2026-05-12 16:43:48 +0530

    Fixed middleware auth path

--- backend/routes/sopRoutes.js
+++ backend/routes/sopRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const Sop = require("../models/sop");
-const auth = require("../middleware/auth");
+const auth = require("../Middleware/auth");
 
 
 // ================= CREATE SOP =================
