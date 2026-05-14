6296e036c0157afa28075c51d1c6d3fc6245ea28
Arghya <arghyadey043@gmail.com>
2026-05-14 14:24:50 +0530

    gemini debug

--- backend/services/aiServices.js
+++ backend/services/aiServices.js
@@ -2,16 +2,15 @@ const {
   GoogleGenerativeAI,
 } = require("@google/generative-ai");
 
-const apiKey =
-  process.env.GEMINI_API_KEY;
-
 console.log(
-  "GEMINI KEY EXISTS:",
-  !!apiKey
+  "GEMINI KEY:",
+  process.env.GEMINI_API_KEY
 );
 
 const genAI =
-  new GoogleGenerativeAI(apiKey);
+  new GoogleGenerativeAI(
+    process.env.GEMINI_API_KEY
+  );
 
 const askAI = async (prompt) => {
 
@@ -25,18 +24,14 @@ const askAI = async (prompt) => {
     const result =
       await model.generateContent(prompt);
 
-    const response =
-      result.response.text();
-
-    return response;
+    return result.response.text();
 
   } catch (error) {
 
     console.log(
-      "🔥 GEMINI FULL ERROR:"
+      "GEMINI ERROR =>",
+      error
     );
-
-    console.log(error);
 
     return "AI service temporarily unavailable.";
   }
